4a0e88b5c600f97fb12a61d70da58571beadd4f1
github-action-benchmark <github@users.noreply.github.com>
2023-04-06 21:05:03 +0000

    add Rust Benchmark (cargo) benchmark result for 624562662e148730d93964db4d5bbb02477d0a32

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1680815103287,
+  "repoUrl": "https://github.com/Narsil/ggblas",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "patry.nicolas@protonmail.com",
+            "name": "Nicolas Patry",
+            "username": "Narsil"
+          },
+          "committer": {
+            "email": "patry.nicolas@protonmail.com",
+            "name": "Nicolas Patry",
+            "username": "Narsil"
+          },
+          "distinct": true,
+          "id": "624562662e148730d93964db4d5bbb02477d0a32",
+          "message": "Adding simple readme.",
+          "timestamp": "2023-04-06T22:57:38+02:00",
+          "tree_id": "9e59e30e58ec10c0903e3e8f7415ba82e6963b87",
+          "url": "https://github.com/Narsil/ggblas/commit/624562662e148730d93964db4d5bbb02477d0a32"
+        },
+        "date": 1680815101995,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_ggml_n",
+            "value": 1442025,
+            "range": "± 741325",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_ggml_t",
+            "value": 719728,
+            "range": "± 98521",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
